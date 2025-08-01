2dccc9f3a90d7b7b18ce25e8fdc115062d8489a4
Ernesto <ernesto.hegi@gmail.com>
2025-08-01 17:03:40 +0100

    API keys and auto reloads

--- src/scrobbler.ts
+++ src/scrobbler.ts
@@ -1,12 +1,22 @@
+import "dotenv/config";
+
 import express from "express";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const API_KEY = process.env.API_KEY;
+const LAST_FM_AUTH_URL = "https://www.last.fm/api/auth/";
+
+console.log("Starting Scrobbler...");
 
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Scrobbler is running!");
+  res.redirect(`${LAST_FM_AUTH_URL}?api_key=${API_KEY}`);
+});
+
+app.get("/auth", (req, res) => {
+  console.log("Received auth request:", req.query);
 });
 
 app.listen(PORT, () => {
