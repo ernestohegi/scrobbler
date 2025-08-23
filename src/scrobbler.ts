d6f2e224104aa28e99bd8bc992ed9f52f85a7bc8
Ernesto <ernesto.hegi@gmail.com>
2025-08-23 14:38:21 +0100

    Triggers test action (#9)

--- src/scrobbler.ts
+++ src/scrobbler.ts
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-console.log("Starting Scrobbler...");
+console.log("Starting Last.fm Scrobbler...");
 
 app.get("/", HomeRoute);
 
